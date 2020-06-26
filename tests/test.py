16402c218f0e2ebd18df57603368a43b0fc090a1
BadgerBo <0099bo@gmail.com>
2020-06-27 01:23:41 +0300

    Updated test.py

--- tests/test.py
+++ tests/test.py
@@ -2,26 +2,35 @@ import unittest
 from selenium import webdriver
 
 
-URL = 'http://olesyabarsukova.pythonanywhere.com/'
-
-
-class TestWebi(unittest.TestCase):   
+class TestWebi(unittest.TestCase): 
 
     def setUp(self):
-        self.driver = webdriver.Chrome()
+        self.url = 'http://olesyabarsukova.pythonanywhere.com/'
+        self.wd = webdriver.Chrome()
+        self.wd.get(self.url)
 
-    def test_connection(self):
-        self.driver.get(URL)
-        self.assertIn('Olesya Barsukova', self.driver.title)
+    def test_navigation(self):
+        nav_sections = ['experience', 'education', 'skills', 'portfolio', 'contacts']
+        for section in nav_sections:
+            self.wd.find_element_by_xpath('//nav//td[' + str(nav_sections.index(section) + 1) + ']').click()
+            current_ulr = self.wd.current_url[45: -1]
+            self.assertEqual(section, current_ulr)
+        self.wd.find_element_by_xpath('//header/a').click()
+        self.assertEqual('home', self.wd.current_url[45: -1])
 
     def test_language_switch(self):
-        self.driver.get(URL)
-        lang_button = self.driver.find_element_by_xpath('//a/img')
+        title = self.wd.title
+        lang_button = self.wd.find_element_by_xpath('//a/img')
+        lang = lang_button.get_attribute('src')[-6: -4]
+        if lang == 'ru':
+            new_title = 'Олеся Барсукова'
+        elif lang == 'en':
+            new_title = 'Олеся Барсукова'
         lang_button.click()
-        self.assertIn('Олеся Барсукова', self.driver.title)
+        self.assertIn(new_title, self.wd.title)
 
     def tearDown(self):
-        self.driver.close()
+        self.wd.quit()
        
 
 if __name__ == '__main__':
